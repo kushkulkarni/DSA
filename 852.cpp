ecc19c73afad77ce864c1ff5c4ba047ebc4fa9ad
kushkulkarni <kushkkulkarni@gmail.com>
2026-07-07 20:51:00 +0530

    Cleared problem no. 852 LC

---
+++ 852.cpp
@@ -0,0 +1,25 @@
+
+class Solution {
+    #include <vector>
+    public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int start = 1;
+        int end = arr.size() - 2;
+        int mid;
+
+        while(start <= end){
+            mid = start + (end-start)/2;
+            if(arr[mid-1] < arr[mid] && arr[mid+1] < arr[mid]){
+                // peak element found
+                break;
+            } else if(arr[mid-1] < arr[mid]){
+                // in left side of mountain peak, hence move to right
+                start = mid + 1;
+            } else if(arr[mid-1] > arr[mid]){
+                // in right side of mountain peak, hence move to left
+                end = mid -1;
+            }
+        }
+        return mid;
+    }
+};
