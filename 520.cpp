05d768a76ad671676ec0413bd075cd47254f221d
kushkulkarni <kushkkulkarni@gmail.com>
2026-07-08 13:17:30 +0530

    solved problem 520 successfully

---
+++ 520.cpp
@@ -0,0 +1,32 @@
+class Solution {
+    #include <vector>
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        int start = 0;
+        int end = n-1;
+        int mid;
+
+        if(n == 1) return nums[0];
+
+        while(start <= end){
+            mid = start + (end-start)/2;
+
+            if(mid == 0 && nums[1] != nums[0]) return nums[mid];
+            else if(mid == n-1 && nums[n-1] != nums[n-2]) return nums[mid];
+
+            if(nums[mid-1] != nums[mid] && nums[mid] != nums[mid+1]) return nums[mid];
+            else if(mid%2 == 0){
+                if(nums[mid-1] == nums[mid])
+                    
+                    end = mid-1;
+                else start = mid+1;
+            }else{
+                if(nums[mid-1] == nums[mid])
+                    start = mid+1;
+                else end = mid-1;
+            }
+        }
+        return -1;
+    }
+};
